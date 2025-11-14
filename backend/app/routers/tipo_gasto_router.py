e41def9762237602aa4357aa63e642a29fdf2062
Fellipe Almeida <almeidafellipe237@gmail.com>
2025-11-14 20:05:16 -0300

    feat: add CRUDs tipo_gasto

--- backend/app/routers/tipo_gasto_router.py
+++ backend/app/routers/tipo_gasto_router.py
@@ -1,27 +1,34 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.models.tipo_gasto_model import TipoGasto
 from app.schemas.tipo_gasto_schema import TipoGastoCreate, TipoGastoCreateResponse
 from app.database import get_db
-from app.schemas.tipo_gasto_schema import TipoGastoDelete, TipoGastoDeleteResponse
+from app.schemas.tipo_gasto_schema import TipoGastoDeleteResponse
+from app.schemas.tipo_gasto_schema import TipoGastoEditResponse
+from app.schemas.tipo_gasto_schema import TipoGastoEdit
+from app.schemas.tipo_gasto_schema import TipoGastoViewResponse
+from app.schemas.tipo_gasto_schema import TipoGastoListResponse
 
 router = APIRouter(
     prefix="/tipo_gasto",
-    tags=["01. tipo_gasto"]
+    tags=["01. Tipo Gasto"]
 )
 
 # -------------------------- CREATE -------------------------------------
 
 @router.post("", response_model=TipoGastoCreateResponse)
-def criar_tipo_gasto(payload: TipoGastoCreate, db: Session = Depends(get_db)):
-    try:
-        model = TipoGasto(**payload.model_dump()) # model_dump transforma os dados em dicionário python para o sqlalchemy
+def criar_tipo_gasto(tipo_gasto: TipoGastoCreate, db: Session = Depends(get_db)):
+        model = TipoGasto(**tipo_gasto.model_dump()) # model_dump transforma os dados em dicionário python para o sqlalchemy
         db.add(model)
         db.commit()
         db.refresh(model)
-        return TipoGastoCreateResponse(id=model.id, message="Tipo de Gasto criado com sucesso!")
-    except Exception as e:
-        print(e)
+
+        return TipoGastoCreateResponse(
+            id=model.id,
+            nome=model.nome,
+            message="Tipo de Gasto criado com sucesso!")
+
 
 # -------------------------- DELETE -------------------------------------
 
@@ -35,9 +42,45 @@ def delete_tipo_gasto(tipo_gasto_id: int, db: Session = Depends(get_db)):
         db.delete(model)
         db.commit()
         return TipoGastoDeleteResponse(
-            id=model.id,
+            nome=model.nome,
             message="Tipo de Gasto excluido com sucesso!"
         )
 
+# -------------------------- EDIT -------------------------------------
 
+@router.put("/{tipo_gasto_id}", response_model=TipoGastoEditResponse)
+def edit_tipo_gasto(tipo_gasto_id: int, tipo_gasto: TipoGastoEdit, db: Session = Depends(get_db)):
+        model = db.query(TipoGasto).filter(TipoGasto.id == tipo_gasto_id).first()
 
+        if not model:
+            raise HTTPException(status_code=404, detail="Tipo de Gasto não encontrado.")
+
+        for key, value in tipo_gasto.model_dump(exclude_unset=True).items():
+            setattr(model, key, value)
+
+        db.commit()
+        return TipoGastoEditResponse(
+            nome=model.nome,
+            message="Tipo de Gasto atualizado com sucesso!"
+        )
+
+# -------------------------- LIST -------------------------------------
+
+@router.get("/list/{tipo_gasto_id}", response_model=List[TipoGastoListResponse])
+def list_tipo_gasto(db: Session = Depends(get_db)):
+    tipos_gastos = db.query(TipoGasto).all()
+    return tipos_gastos
+
+# -------------------------- VIEW -------------------------------------
+
+@router.get("/{tipo_gasto_id}", response_model=TipoGastoViewResponse)
+def get_tipo_gasto(tipo_gasto_id: int, db: Session = Depends(get_db)):
+        model = db.query(TipoGasto).filter(TipoGasto.id == tipo_gasto_id).first()
+
+        if not model:
+            raise HTTPException(status_code=404, detail="Tipo de Gasto não encontrado.")
+
+        return TipoGastoViewResponse(
+            id=model.id,
+            nome=model.nome
+        )
--- backend/app/schemas/tipo_gasto_schema.py
+++ backend/app/schemas/tipo_gasto_schema.py
@@ -1,25 +1,79 @@
 from pydantic import BaseModel, Field
+from pydantic import ConfigDict
 
-class TipoGastoResponse(BaseModel): # <- BaseModel é a classe base para validação dos dados e a geração de docs
-    id: int
-    nome: str
-
-# --------------------------------------------------------------
-
+# -------------------------- SCHEMA CREATE -------------------------------------
 class TipoGastoCreate(BaseModel):
     nome: str = Field(min_length=3, max_length=100, description="Nome do Tipo de Gasto")
 
+
 class TipoGastoCreateResponse(BaseModel):
     id: int
+    nome: str
     message: str
 
-# --------------------------------------------------------------
+    model_config = ConfigDict(
+        json_schema_extra={
+            'example': {
+                'id': 1,
+                'nome': 'Alimentação',
+                'message': 'Tipo de Gasto criado com sucesso.'
+            }
+        }
+    )
 
-class TipoGastoDelete(BaseModel):
-    id: int = Field(description="Identificador do Tipo de Gasto")
 
+# # -------------------------- SCHEMA DELETE -------------------------------------
 class TipoGastoDeleteResponse(BaseModel):
-    id: int
+    nome: str
     message: str
 
-# --------------------------------------------------------------
+    model_config = ConfigDict(
+        json_schema_extra={
+            'example': {
+                'nome': 'Alimentação',
+                'message': 'Tipo de Gasto excluído com sucesso.'
+            }
+        }
+    )
+
+
+# -------------------------- SCHEMA EDIT -------------------------------------
+class TipoGastoEdit(BaseModel):
+    nome: str = Field(min_length=3, max_length=100, description='Nome do Tipo Gasto')
+
+
+class TipoGastoEditResponse(BaseModel):
+    nome: str
+    message: str
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            'example': {
+                'nome': 'Alimentação',
+                'message': 'Tipo de Gasto editado com sucesso.'
+            }
+        }
+    )
+
+
+# -------------------------- SCHEMA LIST -------------------------------------
+
+class TipoGastoListResponse(BaseModel):
+    id: int
+    nome: str
+
+# -------------------------- SCHEMA VIEW -------------------------------------
+
+class TipoGastoViewResponse(BaseModel):
+    id: int
+    nome: str
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            'example': {
+                'id': 1,
+                'nome': 'Alimentação',
+                'message': 'Dados retornados com sucesso.'
+            }
+        }
+    )
